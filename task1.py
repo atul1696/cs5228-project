d92449550deb975ae4f036cc1c6271c43add74d3
atulp-nus <e0771132@u.nus.edu>
2022-11-03 02:44:25 +0800

    Print table of RMSE scores of train and validation

--- task1.py
+++ task1.py
@@ -19,6 +19,9 @@ from kaggle_submission import create_submission
 
 from preprocessing import DataPreprocessor
 
+from pprint import pprint, pformat
+from tabulate import tabulate
+
 # pd.set_option('display.max_columns', None)
 
 trainX, trainY = read_csv('data/train.csv', ylabel='price')
@@ -55,29 +58,59 @@ if gridsearch:
 else:
     parameters = {}
 
-# regressor = DummyRegressor(strategy="mean")
+# regressor = DummyRegressor(strategy='mean')
 regressor = DecisionTreeRegressor(random_state=0) # The best tuned model
 # regressor = Ridge(random_state=0, alpha=5.0, solver='lsqr', tol=0.01)
 # regressor = MLPRegressor(random_state=0, learning_rate_init=0.1, max_iter=500
 
 parameters = {}
+
 def rmse(Y_true, Y_pred):
     rmse = mean_squared_error(Y_true, Y_pred, squared=False)
     return rmse
-gridsearch_regressor = GridSearchCV(TransformedTargetRegressor(RandomForestRegressor(random_state=0, n_jobs=-1), transformer=MinMaxScaler()), param_grid=parameters, scoring=make_scorer(rmse, greater_is_better=False), n_jobs=-1, cv=10, verbose=3)
 
-results = gridsearch_regressor.fit(trainX, trainY)
+k_fold = 10
+gridsearch_regressor = GridSearchCV(TransformedTargetRegressor(RandomForestRegressor(random_state=0, n_jobs=-1), transformer=MinMaxScaler()), param_grid=parameters, scoring=make_scorer(rmse, greater_is_better=False), n_jobs=-1, cv=k_fold, return_train_score=True, verbose=2)
+gridsearch_regressor.fit(trainX, trainY) 
 
-from pprint import pprint
-pprint(gridsearch_regressor.cv_results_)
-pprint(gridsearch_regressor.best_params_)
+def print_results(gridsearch_regressor):
+    best_estimator_index = gridsearch_regressor.best_index_
+    grid_search_results = gridsearch_regressor.cv_results_
+    k_fold = gridsearch_regressor.cv
 
-regressor = gridsearch_regressor.best_estimator_
-base_regressor = gridsearch_regressor.best_estimator_.regressor_
-feature_importance = sorted(dict(zip(col_names, base_regressor.feature_importances_)).items(), key=lambda k: k[1], reverse=True)
-print(feature_importance)
+    train_scores = []
+    cross_validation_scores = []
+    for i in range(k_fold):
+        train_split_score_key = f'split{i}_train_score'
+        train_scores.append(abs(grid_search_results[train_split_score_key][best_estimator_index]))
+        test_split_score_key = f'split{i}_test_score'
+        cross_validation_scores.append(abs(grid_search_results[test_split_score_key][best_estimator_index]))
 
-regressor.fit(trainX, trainY)
-testY = regressor.predict(testX)
+    rmse_table_rows = [
+        ['Minimum', np.min(train_scores), np.min(cross_validation_scores)],
+        ['Mean', np.mean(train_scores), np.mean(cross_validation_scores)],
+        ['Median', np.median(train_scores), np.median(cross_validation_scores)],
+        ['Maximum', np.max(train_scores), np.max(cross_validation_scores)]
+    ]
+
+    rmse_table = tabulate(rmse_table_rows, headers=['Metric', 'Train', 'Validation'], tablefmt='github', floatfmt='.4f')
+    base_regressor = gridsearch_regressor.best_estimator_.regressor_
+    feature_importance = dict(sorted(zip(col_names, base_regressor.feature_importances_)), key=lambda k: k[1], reverse=True)
+
+    print('\n'.join([
+        'Best hyperparameters:',
+        f'{pformat(gridsearch_regressor.best_params_)}',
+        '',
+        'RMSE score metrics:',
+        f'{rmse_table}',
+        '',
+        'Feature importance:',
+        f'{pformat(feature_importance)}'
+    ]))
+
+print_results(gridsearch_regressor)
+
+best_regressor = gridsearch_regressor.best_estimator_
+testY = best_regressor.predict(testX)
 # testY = gs_regressor.predict(testX)
 create_submission(testY, 'baseline-submission.csv')
