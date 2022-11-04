8c325f3ef61ef84e3a7edf26902d5aa1dacd8467
pritam <pritamdutta89@gmail.com>
2022-11-04 12:18:51 +0800

    Config for AdaBoost

--- src/gridsearch_config.py
+++ src/gridsearch_config.py
@@ -11,7 +11,7 @@ regressor_name = 'random_forest'
 parameters = {
     'regressor__n_estimators': [20, 50, 100, 200, 500],
     'regressor__max_depth': [5, 10, 15, 20, 50],
-    'regressor__min_samples_split': [2, 10, 20, 50, 100],
+    'regressor__min_samples_split': [2, 10, 20, 50, 100]
 }
 regressor = RandomForestRegressor(random_state=0, n_jobs=-1)
 # Save the best regressor parameters manually once we are done with the grid search, so we don't need to repeat it for other experiments.
@@ -32,6 +32,20 @@ parameters = {
 }
 regressor = DecisionTreeRegressor(random_state=0)
 best_param_regressor = DecisionTreeRegressor(random_state=0)
+
+gridsearch_config[regressor_name] = {'parameters': parameters, 'regressor': regressor, 'best_param_regressor': best_param_regressor}
+#############################################################################
+
+########################## AdaBoost Regressor ##########################
+regressor_name = 'ada_boost'
+parameters = {
+    'regressor__n_estimators': [100, 200, 500, 1000],
+    'regressor__learning_rate': [0.001, 0.01, 0.1, 0.5, 1],
+    'regressor__loss': ['linear', 'square', 'exponential']
+}
+estimator = DecisionTreeRegressor(random_state=0, criterion='poisson', max_depth=20, max_features=10, min_samples_leaf=3, min_samples_split=2)
+regressor = AdaBoostRegressor(base_estimator=estimator, random_state=0)
+best_param_regressor = AdaBoostRegressor(base_estimator=estimator, random_state=0)
 
 gridsearch_config[regressor_name] = {'parameters': parameters, 'regressor': regressor, 'best_param_regressor': best_param_regressor}
 #############################################################################
