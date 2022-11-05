eb13afe0d1e01c790c39db2919194104f3206136
prakharg24 <prakhargannu@gmail.com>
2022-11-05 15:32:33 +0800

    task 2 new features

--- src/recommendation_utils.py
+++ src/recommendation_utils.py
@@ -1,11 +1,8 @@
 import numpy as np
+import math
 
 def get_recommendation_weights(row, X, feature_weightage):
     similarity_dict = {}
-    ## TODO : The comparisons below do not take into account what happens if the input is NaN
-    ## For example, if the input floor level is NaN, we don't want to give higher importance to other
-    ## properties which also have floor level NaN. Instead, we just want to ignore that feature
-    ## (maybe just by giving score '0' for all data points if the input itself doesn't contain that information)
 
     ## Are they in the same subzone?
     similarity_dict['subzone'] = np.array(X['subzone']==row['subzone'])
@@ -15,33 +12,45 @@ def get_recommendation_weights(row, X, feature_weightage):
     ## TODO : Maybe we can remove both subzone and planning area and replace it all with lat, lng distance
 
     ## Are they in the same price bracket?
-    hist, bin_edges = np.histogram(X['price'], bins=100)
-    similarity_dict['price'] = np.digitize(X['price'], bin_edges) == np.digitize(row['price'], bin_edges)
-    ## TODO : Convert price difference from categorical to continuous
+    price_distance = abs(X['price'] - row['price'])
+    price_sigma = 5e3
+    similarity_dict['price'] = np.exp(-price_distance/(2*(price_sigma**2)))
 
     ## Do they have the same property type?
     similarity_dict['property_type'] = np.array(X['property_type']==row['property_type'])
 
     ## Do they have the same number of beds?
     similarity_dict['num_beds'] = np.array(X['num_beds']==row['num_beds'])
-    ## TODO : We can convert this to continous as well.
-    ## But should we? I think if someone is looking for say 3 bedroom apartments,
-    ## it's not likely they'd be open to 2 bedroom or 4 bedroom units. That part is usually not flexible.
+    ## TODO : What about properties with higher num_beds? Does adding them helps?
 
     ## Are they in the same size bracket?
-    hist, bin_edges = np.histogram(X['size_sqft'], bins=100)
-    similarity_dict['size_sqft'] = np.digitize(X['size_sqft'], bin_edges) == np.digitize(row['size_sqft'], bin_edges)
-    ## TODO : Convert size difference from categorical to continuous
+    size_sqft_distance = abs(X['size_sqft'] - row['size_sqft'])
+    size_sqft_sigma = 5e1
+    similarity_dict['size_sqft'] = np.exp(-size_sqft_distance/(2*(size_sqft_sigma**2)))
     ## TODO : Add another feature for 'price per square foot'
 
     ## Do they have the same floor level?
-    similarity_dict['floor_level'] = np.array(X['floor_level']==row['floor_level'])
+    if isinstance(row['floor_level'], float) and math.isnan(row['floor_level']):
+        similarity_dict['floor_level'] = 0
+    else:
+        similarity_dict['floor_level'] = np.array(X['floor_level']==row['floor_level'])
 
     ## Do they have the same level of furnishing?
-    similarity_dict['furnishing'] = np.array(X['furnishing']==row['furnishing'])
+    if row['furnishing']=='unspecified' :
+        similarity_dict['furnishing'] = 0
+    else:
+        similarity_dict['furnishing'] = np.array(X['furnishing']==row['furnishing'])
 
     ## How much time is left in the tenure?
-    ## TODO : Need data preprocessing first. Then we can simply subtract difference between built year and current year from tenure
+    tenure_left_X = X['tenure_duration'].astype(float) - (2022 - X['built_year'].astype(float))
+    tenure_left_row = float(row['tenure_duration']) - (2022 - float(row['built_year']))
+    if isinstance(tenure_left_row, float) and math.isnan(tenure_left_row):
+        similarity_dict['tenure_left'] = 0
+    else:
+        tenure_left_distance = abs(tenure_left_X - tenure_left_row)
+        tenure_left_sigma = 5e1
+        similarity_dict['tenure_left'] = np.exp(-tenure_left_distance/(2*(tenure_left_sigma**2)))
+        similarity_dict['tenure_left'] = np.nan_to_num(similarity_dict['tenure_left'], nan=0)
 
     weights = np.zeros(len(X))
     for ele in similarity_dict:
